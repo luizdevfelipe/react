f4957e6f05e77d2082606eb1541886c3f86d7e41
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-02-03 10:08:01 -0300

    leitura do valor definido no contexto

--- learning-react/src/app/pages/dashboard/Dashboard.tsx
+++ learning-react/src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { LoggedUserContext } from '../../shared/contexts';
 
 export const Dashboard = () => {
+
+    const { userName } = useContext(LoggedUserContext);
+
     return (
         <header>
             <h1>Dashboard</h1>
+            <h2>Welcome to the Dashboard {userName} !</h2>
             <Link to="/login">Go to Login</Link>
         </header>
     );
--- learning-react/src/app/shared/contexts/LoggedUser.tsx
+++ learning-react/src/app/shared/contexts/LoggedUser.tsx
@@ -8,11 +8,11 @@ interface ILoggedUserProviderProps {
     children: React.ReactNode;
 }
 
-const LoggedUserContext = createContext<ILoggedUserContextData>({} as ILoggedUserContextData);
+export const LoggedUserContext = createContext<ILoggedUserContextData>({} as ILoggedUserContextData);
 
 export const LoggedUserProvider: React.FC<ILoggedUserProviderProps> = ({ children }) => {
     return (
-        <LoggedUserContext.Provider value={{ userName: "" }}>
+        <LoggedUserContext.Provider value={{ userName: "Luiz" }}>
             {children}
         </LoggedUserContext.Provider>
     );
