028572585723d24dd8570a43dc93ddf849307833
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-02-06 13:01:22 -0300

    useState com listas

--- learning-react/src/app/pages/dashboard/Dashboard.tsx
+++ learning-react/src/app/pages/dashboard/Dashboard.tsx
@@ -1,16 +1,42 @@
 import { Link } from 'react-router-dom';
 import { useLoggedUser } from '../../shared/hooks';
+import { useCallback, useState } from 'react';
 
 export const Dashboard = () => {
 
     const { userName, logOut } = useLoggedUser();
+    const [list, setList] = useState<string[]>(["Item1", "Item2", "Item3", "Item4"]);
+
+    const handleInputKeyDown = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            if (event.currentTarget.value.trim().length === 0) return;
+
+            const value = event.currentTarget.value.trim();
+
+            setList((oldList) => {
+                if (oldList.includes(value)) return oldList;    
+                return [...oldList, value];
+            });
+            event.currentTarget.value = "";
+        }
+    }, []);
 
     return (
+        <>
         <header>
             <h1>Dashboard</h1>
             {userName !== "" && <h2>Welcome to the Dashboard {userName}!</h2>}
             <Link to="/login">Go to Login</Link>
             {userName !== "" && <button onClick={logOut}>Log Out</button>}
         </header>
+        <main>
+
+            <input onKeyDown={handleInputKeyDown} />
+
+            <ul>
+                {list.map((value, index) => <li key={value}> {index} - {value}</li>)}
+            </ul>
+        </main>
+        </>
     );
 }
