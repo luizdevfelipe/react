bf93f1dc4138e0165123080c878d2f35c2415b3d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-02-07 17:48:15 -0300

    useState com objetos

--- learning-react/src/app/pages/dashboard/Dashboard.tsx
+++ learning-react/src/app/pages/dashboard/Dashboard.tsx
@@ -2,10 +2,16 @@ import { Link } from 'react-router-dom';
 import { useLoggedUser } from '../../shared/hooks';
 import { useCallback, useState } from 'react';
 
+interface IListItem {
+    value: string;
+    isChecked: boolean;
+}
+
+
 export const Dashboard = () => {
 
     const { userName, logOut } = useLoggedUser();
-    const [list, setList] = useState<string[]>(["Item1", "Item2", "Item3", "Item4"]);
+    const [list, setList] = useState<IListItem[]>([]);
 
     const handleInputKeyDown = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === "Enter") {
@@ -14,8 +20,8 @@ export const Dashboard = () => {
             const value = event.currentTarget.value.trim();
 
             setList((oldList) => {
-                if (oldList.includes(value)) return oldList;    
-                return [...oldList, value];
+                if (oldList.some(item => item.value === value)) return oldList;
+                return [...oldList, { value, isChecked: false }];
             });
             event.currentTarget.value = "";
         }
@@ -23,20 +29,37 @@ export const Dashboard = () => {
 
     return (
         <>
-        <header>
-            <h1>Dashboard</h1>
-            {userName !== "" && <h2>Welcome to the Dashboard {userName}!</h2>}
-            <Link to="/login">Go to Login</Link>
-            {userName !== "" && <button onClick={logOut}>Log Out</button>}
-        </header>
-        <main>
+            <header>
+                <h1>Dashboard</h1>
+                {userName !== "" && <h2>Welcome to the Dashboard {userName}!</h2>}
+                <Link to="/login">Go to Login</Link>
+                {userName !== "" && <button onClick={logOut}>Log Out</button>}
+            </header>
+            <main>
 
-            <input onKeyDown={handleInputKeyDown} />
+                <input onKeyDown={handleInputKeyDown} />
+                <p>{list.filter(item => item.isChecked).length} items selected</p>
 
-            <ul>
-                {list.map((value, index) => <li key={value}> {index} - {value}</li>)}
-            </ul>
-        </main>
+                <ul>
+                   {list.map((listItem) => {
+                    return <li key={listItem.value}>
+                        <input 
+                        type="checkbox"
+                        checked={listItem.isChecked}
+                        onChange={() => setList((oldList) => {
+                            return oldList.map(item => {
+                                if (item.value === listItem.value) {
+                                    return { ...item, isChecked: !item.isChecked };
+                                }
+                                return item;
+                            })
+                        })}
+                        />
+                        {listItem.value}
+                    </li>
+                   })}
+                </ul>
+            </main >
         </>
     );
 }
