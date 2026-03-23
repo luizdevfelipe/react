1d3b6903f28b1b2acab7615bba6014f891470d6b
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-03-23 13:20:52 -0300

    execução de query com parâmetros

--- react-apollo-graphql/client/src/App.tsx
+++ react-apollo-graphql/client/src/App.tsx
@@ -5,25 +5,45 @@ import { gql } from '@apollo/client'
 const GET_USERS = gql`
   query GetUsers {
     getUsers {
-      id
       name
       isMarried
     }
   }
 `;
 
+const GET_USE_BY_ID = gql`
+  query getUserById($id: ID!) {
+    getUserById(id: $id) {
+      name
+    }
+  }
+`;
+
 function App() {
 
-  const { data, loading, error } = useQuery(GET_USERS);
+  const {
+    data: getUsersData,
+    loading: getUsersLoading,
+    error: getUsersError
+  } = useQuery(GET_USERS);
+
+  const {
+    data: getUserData,
+    loading: getUserLoading,
+    error: getUserError
+  } = useQuery(GET_USE_BY_ID, { variables: { id: '1' } });
 
   return (
     <>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
+      {getUserLoading && <p>Loading user...</p>}
+      {getUserError && <p>Error: {getUserError.message}</p>}
+      {getUserData && <h1>Chosen User: {getUserData?.getUserById?.name}</h1>}
 
-      <h1>Users</h1>
+      {getUsersLoading && <p>Loading...</p>}
+      {getUsersError && <p>Error: {getUsersError.message}</p>}
+
       <div>
-        {data?.getUsers?.map((user: any) => (
+        {getUsersData?.getUsers?.map((user: any) => (
           <div key={user.id}>
             <p>Name: {user.name}, Married: {user.isMarried ? 'Yes' : 'No'}</p>
           </div>
