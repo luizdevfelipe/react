2e3c42c6413a9e2c5733e247ecb5a53e24d78ec5
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-07-11 14:59:00 -0300

    contador usando useReducer

--- learning-react/src/app/pages/test/Test.tsx
+++ learning-react/src/app/pages/test/Test.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useReducer } from 'react';
 
 export const Test: React.FC = () => {
-    return (
-        <div>
-            teste
-        </div>
-    )
+  const initialState = {
+    count: 0,
+  }
+
+  function reducer(state: { count: number }, action: { type: string }) {
+    switch (action.type) {
+      case 'increment':
+        return {
+          ...state,
+          count: state.count + 1,
+        }
+
+      case 'decrement':
+        return {
+          ...state,
+          count: state.count - 1,
+        }
+
+      default:
+        return state
+    }
+  }
+
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <>
+      <p>{state.count}</p>
+
+      <button onClick={() => dispatch({ type: 'increment' })}>
+        +
+      </button>
+
+      <button onClick={() => dispatch({ type: 'decrement' })}>
+        -
+      </button>
+    </>
+  )
+
 }
